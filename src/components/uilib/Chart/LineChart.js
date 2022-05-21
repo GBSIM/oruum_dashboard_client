4ce6c2d63cd94ed566a99b789c6308c2cfcbde5a
gyubin <gbcompany27@gmail.com>
2022-05-21 23:16:25 +0900

    reactive menubar design

--- src/components/uilib/Chart/LineChart.js
+++ src/components/uilib/Chart/LineChart.js
@@ -18,64 +18,66 @@ export default function LineChart(props) {
     
 
     return (
-        <div className='line-chart-canvas'>
+        <div className='line-chart-container'>
             <div className='period-picker-container'>
                 {periodButtonList}
             </div>
-            {/* <Line
-                className='stock-price-graph'
-                data={{
-                    labels: xdata,
-                    datasets: [
-                        {
-                        backgroundColor: 'rgba(75,192,192,1)',
-                        borderColor: 'rgba(154,216,205,1)',
-                        borderWidth: 2,
-                        data: ydata,
-                        pointRadius: 0
-                        }
-                    ]
-                    }}
-                width="100vw"
-                height="20vh"
-                options={{
-                    plugins: {
-                        legend: {
-                          position: 'top',
-                          display: false,
-                        },
-                        title: {
-                          display: false,
-                          text: 'Stock price',
-                        },
-                      },
-                    scales: {
-                        xAxis: {
+            <div className='line-chart-canvas'>
+                <Line
+                    className='line-chart'
+                    width="100vw"
+                    data={{
+                        labels: xdata,
+                        datasets: [
+                            {
+                            backgroundColor: 'rgba(75,192,192,1)',
+                            borderColor: 'rgba(154,216,205,1)',
+                            borderWidth: 2,
+                            data: ydata,
+                            pointRadius: 0
+                            }
+                        ]
+                        }}
+                    options={{
+                        // responsive: false,
+                        maintainAspectRatio: false,
+                        plugins: {
+                            legend: {
+                            position: 'top',
                             display: false,
-                        },
-                        yAxis: {
+                            },
+                            title: {
                             display: false,
-                        },
-                        x: {
-                            ticks: {
-                                maxRotation: 0,
-                                minRotation: 0,
-                                font: {
-                                    size: 14,
-                                    family: 'Noto Sans KR',
-                                    weight: 500,
-                                },
-                                align: 'start',
-                                // For a category axis, the val is the index so the lookup via getLabelForValue is needed
-                                callback: function(val, index) {
-                                    return index%20 === 0? this.getLabelForValue(index): "";
-                                },
+                            text: 'Stock price',
                             },
                         },
-                    }}}
-                 /> */}
-        </div>
-        
+                        scales: {
+                            xAxis: {
+                                display: false,
+                            },
+                            yAxis: {
+                                display: false,
+                            },
+                            x: {
+                                ticks: {
+                                    maxRotation: 0,
+                                    minRotation: 0,
+                                    font: {
+                                        size: 14,
+                                        family: 'Noto Sans KR',
+                                        weight: 500,
+                                    },
+                                    align: 'start',
+                                    // For a category axis, the val is the index so the lookup via getLabelForValue is needed
+                                    callback: function(val, index) {
+                                        return index%20 === 0? this.getLabelForValue(index): "";
+                                    },
+                                },
+                            },
+                        }}}
+                    />
+            </div>
+        </div>        
     )
 }
 
